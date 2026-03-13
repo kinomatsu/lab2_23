b4dad50152cf9a678fcb102eaac948ab9e0338ba
kinomatsu <vany56999@yandex.com>
2026-03-14 00:59:20 +0300

    Логика

--- lab2_23/Form1.Designer.cs
+++ lab2_23/Form1.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -76,6 +76,42 @@
             this.tabPage1.Text = "Средний балл";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Средний балл:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Рассчитать средний";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 65);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 97);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите оценки через пробел:";
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.listBox1);
@@ -89,6 +125,41 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Анализ оценок";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(58, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(387, 160);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выполнить анализ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 72);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 101);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Введите оценки через пробел:";
             // 
             // tabPage3
             // 
@@ -104,93 +175,13 @@
             this.tabPage3.Text = "Диаграмма";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Введите оценки через пробел:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(52, 65);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(266, 97);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(52, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 65);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Рассчитать средний";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 316);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Средний балл:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(55, 37);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Введите оценки через пробел:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(58, 72);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(262, 101);
-            this.textBox2.TabIndex = 1;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(58, 193);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(262, 49);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Выполнить анализ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(58, 260);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(387, 160);
-            this.listBox1.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 23);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Введите оценки через пробел:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(41, 74);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(284, 110);
-            this.textBox3.TabIndex = 1;
+            this.pictureBox1.Location = new System.Drawing.Point(360, 207);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 213);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // button3
             // 
@@ -202,13 +193,22 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // pictureBox1
+            // textBox3
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(370, 246);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(240, 157);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.textBox3.Location = new System.Drawing.Point(41, 74);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(284, 110);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Введите оценки через пробел:";
             // 
             // Form1
             // 
--- lab2_23/Form1.cs
+++ lab2_23/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lab2_23
@@ -15,6 +10,126 @@ namespace lab2_23
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // =========================
+        // 1. Средний балл
+        // =========================
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] marks = textBox1.Text
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                double avg = marks.Average();
+
+                label2.Text = "Средний балл: " + avg.ToString("F2");
+            }
+            catch
+            {
+                MessageBox.Show("Введите оценки через пробел!");
+            }
+        }
+
+        // =========================
+        // 2. Анализ оценок
+        // =========================
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+
+                int[] marks = textBox2.Text
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int count5 = marks.Count(x => x == 5);
+                int count4 = marks.Count(x => x == 4);
+                int count3 = marks.Count(x => x == 3);
+                int count2 = marks.Count(x => x == 2);
+
+                listBox1.Items.Add("5 - " + count5);
+                listBox1.Items.Add("4 - " + count4);
+                listBox1.Items.Add("3 - " + count3);
+                listBox1.Items.Add("2 - " + count2);
+
+                int total = marks.Length;
+                int passed = marks.Count(x => x >= 3);
+
+                double percent = (double)passed / total * 100;
+
+                listBox1.Items.Add("");
+                listBox1.Items.Add("Успеваемость: " + percent.ToString("F1") + "%");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка ввода!");
+            }
+        }
+
+        // =========================
+        // 3. Диаграмма
+        // =========================
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] marks = textBox3.Text
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int count5 = marks.Count(x => x == 5);
+                int count4 = marks.Count(x => x == 4);
+                int count3 = marks.Count(x => x == 3);
+                int count2 = marks.Count(x => x == 2);
+
+                int total = marks.Length;
+
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(bmp);
+
+                g.Clear(Color.White);
+
+                float startAngle = 0;
+
+                DrawSector(g, count5, total, ref startAngle, Brushes.Green);
+                DrawSector(g, count4, total, ref startAngle, Brushes.Blue);
+                DrawSector(g, count3, total, ref startAngle, Brushes.Orange);
+                DrawSector(g, count2, total, ref startAngle, Brushes.Red);
+
+                pictureBox1.Image = bmp;
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка ввода!");
+            }
+        }
+
+        private void DrawSector(Graphics g, int value, int total, ref float startAngle, Brush brush)
+        {
+            if (value == 0) return;
+
+            float sweep = (float)value / total * 360;
+
+            g.FillPie(
+                brush,
+                10,
+                10,
+                200,
+                200,
+                startAngle,
+                sweep);
+
+            startAngle += sweep;
         }
     }
 }
